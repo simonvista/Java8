d84f532ada0ef1955a9efa74a9f6efe932f0baef
YL <simon-ling@hotmail.com>
2021-09-21 23:21:47 -0400

    done

---
+++ src/main/java/com/javatechie/stream/api/example/DataBase.java
@@ -0,0 +1,18 @@
+package com.javatechie.stream.api.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//DAO layer
+public class DataBase {
+	public static List<Employee> getEmployees(){
+		List<Employee> list=new ArrayList<Employee>();
+		list.add(new Employee(176,"Roshan","IT",600000));
+		list.add(new Employee(388, "Bikash","Civil", 900000));
+		list.add(new Employee(470 , "Bimal","Defence",500000 ));
+		list.add(new Employee( 624, "Sourav","Core",400000 ));
+		list.add(new Employee( 176, "Prakash","Social", 1200000));
+		return list;
+	}
+	
+}
---
+++ src/main/java/com/javatechie/stream/api/example/TaxService.java
@@ -0,0 +1,16 @@
+package com.javatechie.stream.api.example;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaxService {
+	public static List<Employee> evaluateTaxUsers(){
+		return DataBase.getEmployees()
+						.stream()
+						.filter(emp->emp.getSalary()>500000)
+						.collect(Collectors.toList());
+	}
+	public static void main(String[] args) {
+		System.out.println(evaluateTaxUsers());
+	}
+}
