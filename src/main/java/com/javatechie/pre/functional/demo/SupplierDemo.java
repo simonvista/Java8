c24476e58d6f02c7555f1821609b7f2bb21771b5
YL <simon-ling@hotmail.com>
2021-09-21 22:29:31 -0400

    done

--- src/main/java/com/javatechie/pre/functional/demo/SupplierDemo.java
+++ src/main/java/com/javatechie/pre/functional/demo/SupplierDemo.java
@@ -1,15 +1,18 @@
 package com.javatechie.pre.functional.demo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
-public class SupplierDemo implements Supplier<String> {
+public class SupplierDemo {
 
-	@Override
-	public String get() {
-		return "Hi JavaTechie";
-	}
 	public static void main(String[] args) {
-		Supplier<String> supplier=new SupplierDemo();
+		Supplier<String> supplier=()->"Hi JavaTechie";
 		System.out.println(supplier.get());
+		
+		//List<String> list1=Arrays.asList("a","b","c","d");	//a
+		List<String> list1=Arrays.asList();						//Hi JavaTechie
+		//orElseGet() uses Supplier as parameter
+		System.out.println(list1.stream().findAny().orElseGet(supplier));
 	}
 }
