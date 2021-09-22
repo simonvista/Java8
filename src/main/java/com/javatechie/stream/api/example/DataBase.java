98c347520e40ac0f6314790ac6cf76ae5e01e31c
YL <simon-ling@hotmail.com>
2021-09-22 17:47:06 -0400

    done

--- src/main/java/com/javatechie/stream/api/example/DataBase.java
+++ src/main/java/com/javatechie/stream/api/example/DataBase.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 //DAO layer
 public class DataBase {
-	public static List<Employee> getEmployees(){
-		List<Employee> list=new ArrayList<Employee>();
-		list.add(new Employee(176,"Roshan","IT",600000));
-		list.add(new Employee(388, "Bikash","Civil", 900000));
-		list.add(new Employee(470 , "Bimal","Defence",500000 ));
-		list.add(new Employee( 624, "Sourav","Core",400000 ));
-		list.add(new Employee( 176, "Prakash","Social", 1200000));
+	public static List<Employee> getEmployees() {
+		List<Employee> list = new ArrayList<Employee>();
+		list.add(new Employee(176, "Roshan", "IT", 600000));
+		list.add(new Employee(388, "Bikash", "Civil", 900000));
+		list.add(new Employee(470, "Bimal", "Defence", 500000));
+		list.add(new Employee(624, "Sourav", "Core", 400000));
+		list.add(new Employee(176, "Prakash", "Social", 1200000));
 		return list;
 	}
-	
+
 }
--- src/main/java/com/javatechie/stream/sort/SortMapDemo.java
+++ src/main/java/com/javatechie/stream/sort/SortMapDemo.java
@@ -3,6 +3,8 @@ package com.javatechie.stream.sort;
 import java.util.*;
 import java.util.Map.Entry;
 
+import com.javatechie.stream.api.example.Employee;
+
 public class SortMapDemo {
 	public static void main(String[] args) {
 		Map<String, Integer> map=new HashMap();
@@ -37,6 +39,21 @@ public class SortMapDemo {
 //		map.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(x->System.out.println(x.getKey()+", "+x.getValue()));
 		map.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(x->System.out.println(x.getKey()+", "+x.getValue()));
 		
+		//more complex map
+		System.out.println("---------------------------------------------------------");
+		Map<Employee,Integer> employeeMap=new TreeMap<>(new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				return (int) (o1.getSalary()-o2.getSalary());
+			}
+		});
+		employeeMap.put(new Employee(176,"Roshan","IT",600000), 60);
+		employeeMap.put(new Employee(388, "Bikash","Civil", 900000), 90);
+		employeeMap.put(new Employee(470, "Bimal","Defence",500000), 50);
+		employeeMap.put(new Employee( 624, "Sourav","Core",400000), 40);
+		employeeMap.put(new Employee( 176, "Prakash","Social", 1200000), 120);
+		System.out.println(employeeMap);
 		
 	}
 }
