51e06a8913f3e729306e62d4a1ba8e74f706cc77
YL <simon-ling@hotmail.com>
2021-09-22 17:25:12 -0400

    done

--- src/main/java/com/javatechie/stream/sort/SortMapDemo.java
+++ src/main/java/com/javatechie/stream/sort/SortMapDemo.java
@@ -15,18 +15,27 @@ public class SortMapDemo {
 		//sort by key in ascending
 		//1. convert map to list since Collections.sort() works on list
 		List<Entry<String,Integer>> entries=new ArrayList<>(map.entrySet());
-		//2. sort		
-		Collections.sort(entries,new Comparator<Entry<String,Integer>>() {
-
-			@Override
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				return o1.getKey().compareTo(o2.getKey());
-			}			
-		});
+		//2.1 sort-traditional way		
+//		Collections.sort(entries,new Comparator<Entry<String,Integer>>() {
+//
+//			@Override
+//			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+//				return o1.getKey().compareTo(o2.getKey());
+//			}			
+//		});
+		//2.2 sort-lambda expression
+		Collections.sort(entries, (o1,o2)->o1.getKey().compareTo(o2.getKey()));
+		
 		//3. parse list
 		for(Entry<String,Integer> entry:entries) {
 			System.out.println(entry.getKey()+", "+entry.getValue());
 		}
+		System.out.println("---------------------------------------------------------");
+		//use stream() to sort set
+//		map.entrySet().stream().sorted((o1,o2)->o1.getKey().compareTo(o2.getKey())).forEach(x->System.out.println(x.getKey()+", "+x.getValue()));
+//		System.out.println("---------------------------------------------------------");
+//		map.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(x->System.out.println(x.getKey()+", "+x.getValue()));
+		map.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(x->System.out.println(x.getKey()+", "+x.getValue()));
 		
 		
 	}
