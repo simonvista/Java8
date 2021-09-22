acd03a60d09ebd986a2e9d3c86a78b7c7890c339
YL <simon-ling@hotmail.com>
2021-09-21 20:07:48 -0400

    done

--- src/main/java/com/javatechie/java8_tutorial/CalculatorImpl.java
+++ src/main/java/com/javatechie/java8_tutorial/CalculatorImpl.java
@@ -1,13 +1,17 @@
 package com.javatechie.java8_tutorial;
 
+//@FunctionalInterface
 interface Calculator{
-	void switchOn();
+	//void switchOn();
+	void sum(int input);
 }
 
 public class CalculatorImpl {
 	
 	public static void main(String[] args) {
-		Calculator calculator=()->System.out.println("SwithOn method was called");
-		calculator.switchOn();
+//		Calculator calculator=()->System.out.println("SwithOn method was called");
+//		calculator.switchOn();
+		Calculator calculator=input->System.out.println("sum: "+input);
+		calculator.sum(2);
 	}
 }
