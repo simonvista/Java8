693929982a099b81a4869a68b114d4087ef67874
YL <simon-ling@hotmail.com>
2021-09-21 22:32:15 -0400

    done

--- src/main/java/com/javatechie/pre/functional/demo/SupplierDemo.java
+++ src/main/java/com/javatechie/pre/functional/demo/SupplierDemo.java
@@ -6,13 +6,11 @@ import java.util.function.Supplier;
 
 public class SupplierDemo {
 
-	public static void main(String[] args) {
-		Supplier<String> supplier=()->"Hi JavaTechie";
-		System.out.println(supplier.get());
+	public static void main(String[] args) {		
 		
 		//List<String> list1=Arrays.asList("a","b","c","d");	//a
 		List<String> list1=Arrays.asList();						//Hi JavaTechie
 		//orElseGet() uses Supplier as parameter
-		System.out.println(list1.stream().findAny().orElseGet(supplier));
+		System.out.println(list1.stream().findAny().orElseGet(()->"Hi JavaTechie"));
 	}
 }
