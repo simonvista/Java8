924b61964b4a052ee8d236c2b7f86d35c7b5f13e
YL <simon-ling@hotmail.com>
2021-09-21 21:46:04 -0400

    done

---
+++ src/main/java/com/javatechie/pre/functional/demo/ConsumerDemo.java
@@ -0,0 +1,15 @@
+package com.javatechie.pre.functional.demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+	
+	public static void main(String[] args) {
+		Consumer<Integer> consumer=t->System.out.println("print "+t);
+		consumer.accept(-1);
+		List<Integer> list1=Arrays.asList(1,2,3,4,5);
+		list1.stream().forEach(consumer);
+	}
+}
