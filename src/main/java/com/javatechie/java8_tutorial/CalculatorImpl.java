ab50df5ebec8020baecebfabf86ce8018ae40e02
YL <simon-ling@hotmail.com>
2021-09-21 20:19:24 -0400

    done

--- src/main/java/com/javatechie/java8_tutorial/CalculatorImpl.java
+++ src/main/java/com/javatechie/java8_tutorial/CalculatorImpl.java
@@ -3,7 +3,9 @@ package com.javatechie.java8_tutorial;
 //@FunctionalInterface
 interface Calculator{
 	//void switchOn();
-	void sum(int input);
+	
+//	void sum(int input);
+	int substract(int i1,int i2);
 }
 
 public class CalculatorImpl {
@@ -11,7 +13,19 @@ public class CalculatorImpl {
 	public static void main(String[] args) {
 //		Calculator calculator=()->System.out.println("SwithOn method was called");
 //		calculator.switchOn();
-		Calculator calculator=input->System.out.println("sum: "+input);
-		calculator.sum(2);
+		
+//		Calculator calculator=input->System.out.println("sum: "+input);
+//		calculator.sum(2);
+		
+//		Calculator calculator=(i1,i2)->i1-i2;
+		Calculator calculator=(i1,i2)->{
+			if(i2<i1) {
+				throw new RuntimeException("i2 should not be smaller than i1");
+			}
+			else {
+				return i2-i1;
+			}
+		};
+		System.out.println(calculator.substract(30, 9)); 
 	}
 }
