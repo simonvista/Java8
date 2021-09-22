297ff1882f45e24366a8e34017a72d911327e263
YL <simon-ling@hotmail.com>
2021-09-21 21:21:47 -0400

    done

--- src/main/java/com/javatechie/lambda/example/BookService.java
+++ src/main/java/com/javatechie/lambda/example/BookService.java
@@ -7,30 +7,13 @@ import java.util.List;
 public class BookService {
 	public List<Book> getBooksSortedByName(){
 		List<Book> books=new BookDAO().getBooks();
-//		Collections.sort(books, new MyComparator());
-		Collections.sort(books, new Comparator<Book>() {
-
-			@Override
-			public int compare(Book o1, Book o2) {
-				//ascending order
-				//return o1.getName().compareTo(o2.getName());
-				//descending order
-				return o2.getName().compareTo(o1.getName());
-			}
-		});
+		//ascending order
+		//Collections.sort(books, (o1,o2)->o1.getName().compareTo(o2.getName()));
+		//descending order
+		Collections.sort(books, (o1,o2)->o2.getName().compareTo(o1.getName()));
 		return books;
 	}
 	public static void main(String[] args) {
 		System.out.println(new BookService().getBooksSortedByName());
 	}
 }
-
-/*
- * class MyComparator implements Comparator<Book>{
- * 
- * @Override public int compare(Book o1, Book o2) { //ascending order //return
- * o1.getName().compareTo(o2.getName()); //ascending order return
- * o2.getName().compareTo(o1.getName()); }
- * 
- * }
- */
