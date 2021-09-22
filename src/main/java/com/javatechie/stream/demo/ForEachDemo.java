dafe71d8c54487c861535db55224b8ddc923c9ff
YL <simon-ling@hotmail.com>
2021-09-21 22:59:25 -0400

    done

--- src/main/java/com/javatechie/stream/demo/ForEachDemo.java
+++ src/main/java/com/javatechie/stream/demo/ForEachDemo.java
@@ -8,14 +8,14 @@ import java.util.Map;
 public class ForEachDemo {
 
 	public static void main(String[] args) {
-		//forEach
+		//filter
 		List<String> list=new ArrayList<>();
 		list.add("Murrit");
 		list.add("John");
 		list.add("Piter");
 		list.add("Marek");
 		list.add("Mac");
-		list.stream().forEach(System.out::println);
+		list.stream().filter(x->x.startsWith("M")).forEach(System.out::println);
 		
 		Map<Integer,String> map=new HashMap<>();
 		map.put(1, "a");
@@ -23,7 +23,7 @@ public class ForEachDemo {
 		map.put(3, "c");
 		map.put(4, "d");
 		map.forEach((k,v)->System.out.println(k+", "+v));
-		map.entrySet().stream().forEach(obj->System.out.println(obj));
+		map.entrySet().stream().filter(x->x.getKey()%2==0).forEach(obj->System.out.println(obj));
 	}
 
 }
